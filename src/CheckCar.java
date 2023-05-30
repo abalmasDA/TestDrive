2abc4d5d788faaf26e23a30403fc6a4d28490491
abalmasDA <abalmasit@gmail.com>
2023-05-30 13:53:56 +0300

    Commit2

--- src/CheckCar.java
+++ src/CheckCar.java
@@ -1,7 +1,6 @@
 public class CheckCar {
     private Car[] listCarToCheck = new Car[0];
 
-
     public void addingCarToCheckList(Car carToAdd) {
         Car[] resultAddCar = new Car[listCarToCheck.length + 1];
         for (int i = 0; i < listCarToCheck.length; i++) {
@@ -11,26 +10,47 @@ public class CheckCar {
         listCarToCheck = resultAddCar;
     }
 
-//    public Car findFastestCar() {
-//
-//        for (int i = 1; i < listCarToCheck.length; i++) {
-//            if (listCarToCheck[i].getSpeed() > listCarToCheck[i].getSpeed()) {
-//
-//
-//            }
-//
-//        }
-//
-//
-//        return;
-//    }
+    public String findFastestCar() {
+        int maxSpeed = listCarToCheck[0].getSpeed();
+        int counterIndex = 0;
+        for (int i = 0; i < listCarToCheck.length; i++) {
+            if (listCarToCheck[i].getSpeed() > maxSpeed) {
+                maxSpeed = listCarToCheck[i].getSpeed();
+                counterIndex = i;
+            }
+        }
+        return outputCarInfo(listCarToCheck[counterIndex]);
+    }
 
+    public String findMostExpensiveCar() {
+        double maxPrice = listCarToCheck[0].getPrice();
+        int counterIndex = 0;
+        for (int i = 0; i < listCarToCheck.length; i++) {
+            if (listCarToCheck[i].getPrice() > maxPrice) {
+                maxPrice = listCarToCheck[i].getPrice();
+                counterIndex = i;
+            }
+        }
+        return outputCarInfo(listCarToCheck[counterIndex]);
+    }
+
+    public String findMostPowerfulCar() {
+        double maxPower = listCarToCheck[0].getPower();
+        int counterIndex = 0;
+        for (int i = 0; i < listCarToCheck.length; i++) {
+            if (listCarToCheck[i].getPower() > maxPower) {
+                maxPower = listCarToCheck[i].getPower();
+                counterIndex = i;
+            }
+        }
+        return outputCarInfo(listCarToCheck[counterIndex]);
+    }
 
     public String outputCarInfo(Car carInfoOutput) {
         return "Brand: " + carInfoOutput.getModel() + "\n" +
-                "Power: " + carInfoOutput.getPower() + "\n" +
+                "Power: " + (int) carInfoOutput.getPower() + "\n" +
                 "Speed: " + carInfoOutput.getSpeed() + "\n" +
-                "Price: " + carInfoOutput.getPrice() + "\n" +
+                "Price:" + String.format("%.2f", carInfoOutput.getPrice()).replace(',', '.') + "\n" +
                 "Year: " + carInfoOutput.getYearOfManufacture();
     }
 
--- src/Main.java
+++ src/Main.java
@@ -12,16 +12,21 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Car ford = new Car("FORD Mustang", 250, 330, 75000);
-        Car mitsubishi = new Car("MITSUBISHI Lancer", 150, 270, 45000);
-        Car toyota = new Car("TOYOTA Camry", 150, 220, 45000);
-        Car kia = new Car("KIA Rio", 150, 200, 45000);
+        Car ford = new Car("FORD Mustang", 175, 250, 43800);
+        Car mitsubishi = new Car("MITSUBISHI Lancer", 120, 190, 17000);
+        Car toyota = new Car("TOYOTA Camry", 145, 200, 25000);
+        Car kia = new Car("KIA Rio", 125, 185, 15000);
 
         CheckCar checkCar = new CheckCar();
         checkCar.addingCarToCheckList(ford);
         checkCar.addingCarToCheckList(mitsubishi);
         checkCar.addingCarToCheckList(toyota);
         checkCar.addingCarToCheckList(kia);
+
+        System.out.println(checkCar.findFastestCar());
+        System.out.println(checkCar.findMostExpensiveCar());
+        System.out.println(checkCar.findMostPowerfulCar());
+
 
 
 
